9fdfd496e7833548847c86e5f4a243809f008e42
Maryaja <Maryajarr@gmail.com>
2024-09-23 22:56:17 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/Header'
+import Aside from './components/Aside'
+import Footer from './components/Footer'
+import Content from './components/content'
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+    <Header/>
+    <Content/>
+    <Aside/>
+    <Footer/>
+
+     
+
     </div>
-  );
+  )
 }
 
-export default App;
+
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>© 2024 Tu Empresa. Todos los derechos reservados.</p>
+    </div>
+  );
+}
+
+export default Footer;
